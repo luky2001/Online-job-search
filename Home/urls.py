44ab24f1779468a720cf4ed59d844d2c29295719
Rohit <luckykumarpatel2001@gmail.com>
2025-01-16 23:43:53 +0530

    first

--- Home/urls.py
+++ Home/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('delete/<int:id>/',views.delete,name='delete'),
     path('update/<int:id>/',views.update,name='update'),
     path('show/',views.show,name='show'),
+    path('login/',views.login_page,name='login'),
+    path('register/',views.register,name='register'),
+    path('logout/',views.logout_page,name="logout"),
 ]
